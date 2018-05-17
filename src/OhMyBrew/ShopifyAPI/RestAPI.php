21810f395056303299a321c85b58921acd64228e
Tyler King <tyler.n.king@gmail.com>
2018-05-17 12:19:05 -0230

    Basis of graph support

---
+++ src/OhMyBrew/ShopifyAPI/RestAPI.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace OhMyBrew\ShopifyAPI;
+
+use Exception;
+
+/**
+ * REST Shopify API.
+ */
+class RestAPI extends BaseAPI
+{
+    /**
+     * Constructor.
+     *
+     * @param bool $private If this is a private or public app
+     *
+     * @return self
+     */
+    public function __construct(bool $private = false)
+    {
+        parent::__construct($private);
+
+        // Create default placeholder
+        $this->apiCallLimits = ['left' => 0, 'made' => 0, 'limit' => 40];
+
+        return $this;
+    }
+
+    /**
+     * Runs a request to the Shopify API.
+     *
+     * @param string     $type   The type of request... GET, POST, PUT, DELETE
+     * @param string     $path   The Shopify API path... /admin/xxxx/xxxx.json
+     * @param array|null $params Optional parameters to send with the request
+     *
+     * @return array An array of the Guzzle response, and JSON-decoded body
+     */
+    public function request(string $type, string $path, array $params = null)
+    {
+        // Build the request parameters for Guzzle
+        $guzzleParams = ['headers' => ['X-Shopify-Access-Token' => $this->accessToken]];
+        $guzzleParams[strtoupper($type) === 'GET' ? 'query' : 'json'] = $params;
+
+        // Create the request, pass the access token and optional parameters
+        $response = $this->client->request(
+            $type,
+            $this->getBaseUrl().$path,
+            $guzzleParams
+        );
+
+        // Grab the API call limit header returned from Shopify
+        $calls = explode('/', $response->getHeader('http_x_shopify_shop_api_call_limit')[0]);
+        $callsMade = $calls[0];
+        $callsLimit = $calls[1];
+
+        // Set it into the class
+        $this->apiCallLimits = [
+            'left'  => (int) $callsLimit - $callsMade,
+            'made'  => (int) $callsMade,
+            'limit' => (int) $callsLimit,
+        ];
+
+        // From firebase/php-jwt
+        $body = $response->getBody();
+        if (!(defined('JSON_C_VERSION') && PHP_INT_SIZE > 4)) {
+            /**
+             * In PHP >=5.4.0, json_decode() accepts an options parameter, that allows you
+             * to specify that large ints (like Steam Transaction IDs) should be treated as
+             * strings, rather than the PHP default behaviour of converting them to floats.
+             */
+            $bodyObj = json_decode($body, false, 512, JSON_BIGINT_AS_STRING);
+        } else {
+            // @codeCoverageIgnoreStart
+            /**
+             * Not all servers will support that, however, so for older versions we must
+             * manually detect large ints in the JSON string and quote them (thus converting
+             * them to strings) before decoding, hence the preg_replace() call.
+             * Currently not sure how to test this so I ignored it for now.
+             */
+            $maxIntLength = strlen((string) PHP_INT_MAX) - 1;
+            $jsonWithoutBigints = preg_replace('/:\s*(-?\d{'.$maxIntLength.',})/', ': "$1"', $body);
+            $bodyObj = json_decode($jsonWithoutBigints);
+            // @codeCoverageIgnoreEnd
+        }
+
+        // Return Guzzle response and JSON-decoded body
+        return (object) [
+            'response' => $response,
+            'body'     => $bodyObj,
+        ];
+    }
+
+    /**
+     * Returns the current API call limits.
+     *
+     * @param string|null $key The key to grab (left, made, limit)
+     *
+     * @throws \Exception When attempting to grab a key that doesn't exist
+     *
+     * @return array An array of the Guzzle response, and JSON-decoded body
+     */
+    public function getApiCalls(string $key = null)
+    {
+        if ($key) {
+            if (!in_array($key, ['left', 'made', 'limit'])) {
+                // No key like that in array
+                throw new Exception('Invalid API call limit key. Valid keys are: '.implode(', ', array_keys($this->apiCallLimits)));
+            }
+
+            // Return the key value requested
+            return $this->apiCallLimits[$key];
+        }
+
+        // Return all the values
+        return $this->apiCallLimits;
+    }
+
+    /**
+     * Gets the base URL to use depending on if its a privte or public app.
+     *
+     * @throws \Exception When missing API key or API password for private apps
+     * @throws \Exception When missing Shopify domain
+     *
+     * @return string The final base URL to use with the API
+     */
+    public function getBaseUrl() : string
+    {
+        if ($this->isPrivate && ($this->apiKey === null || $this->apiPassword === null)) {
+            // Private apps need key and password
+            throw new Exception('API key and password required for private Shopify API calls');
+        }
+
+        if ($this->shop === null) {
+            // Public and private apps need domain regardless
+            throw new Exception('Shopify domain missing for API calls');
+        }
+
+        return $this->isPrivate ? "https://{$this->apiKey}:{$this->apiPassword}@{$this->shop}" : "https://{$this->shop}";
+    }
+}
---
+++ test/BaseApiTest.php
@@ -0,0 +1,278 @@
+<?php
+
+namespace OhMyBrew\ShopifyAPI;
+
+use ReflectionClass;
+use Exception;
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Response;
+
+// Used for testing BaseAPI
+class TestAPI extends BaseAPI
+{
+    // ...
+}
+
+class BaseApiTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @test
+     *
+     * Should set API to private mode
+     */
+    public function itShouldSetApiToPrivateMode()
+    {
+        $api = new TestAPI(true);
+        $reflected = new ReflectionClass($api);
+
+        $privateProperty = $reflected->getProperty('isPrivate');
+        $privateProperty->setAccessible(true);
+
+        $this->assertEquals(true, $privateProperty->getValue($api));
+    }
+
+    /**
+     * @test
+     *
+     * Should set API to public mode
+     */
+    public function itShouldSetApiToPublicMode()
+    {
+        $api = new TestAPI(false);
+        $reflected = new ReflectionClass($api);
+
+        $privateProperty = $reflected->getProperty('isPrivate');
+        $privateProperty->setAccessible(true);
+
+        $this->assertEquals(false, $privateProperty->getValue($api));
+    }
+
+    /**
+     * @test
+     *
+     * Should set shop
+     */
+    public function itShouldSetShop()
+    {
+        $api = new TestAPI();
+        $api->setShop('example.myshopify.com');
+
+        $this->assertEquals('example.myshopify.com', $api->getShop());
+    }
+
+    /**
+     * @test
+     *
+     * Should set access token
+     */
+    public function itShouldSetAccessToken()
+    {
+        $api = new TestAPI();
+        $api->setAccessToken('123');
+
+        $this->assertEquals('123', $api->getAccessToken());
+    }
+
+    /**
+     * @test
+     *
+     * Should set API key and API password and API shared secret
+     */
+    public function itShouldSetApiKeyAndPassword()
+    {
+        $api = new TestAPI();
+        $api->setApiKey('123');
+        $api->setApiPassword('abc');
+        $api->setApiSecret('!@#');
+
+        $reflected = new ReflectionClass($api);
+
+        $apiKeyProperty = $reflected->getProperty('apiKey');
+        $apiKeyProperty->setAccessible(true);
+
+        $apiPasswordProperty = $reflected->getProperty('apiPassword');
+        $apiPasswordProperty->setAccessible(true);
+
+        $apiSecretProperty = $reflected->getProperty('apiSecret');
+        $apiSecretProperty->setAccessible(true);
+
+        $this->assertEquals('123', $apiKeyProperty->getValue($api));
+        $this->assertEquals('abc', $apiPasswordProperty->getValue($api));
+        $this->assertEquals('!@#', $apiSecretProperty->getValue($api));
+    }
+
+    /**
+     * @test
+     *
+     * Should allow for own client injection
+     */
+    public function itShouldAllowForOwnClient()
+    {
+        $api = new TestAPI();
+        $api->setClient(new Client(['handler' => new MockHandler()]));
+
+        $reflected = new ReflectionClass($api);
+
+        $clientProperty = $reflected->getProperty('client');
+        $clientProperty->setAccessible(true);
+
+        $value = $clientProperty->getValue($api);
+
+        $this->assertEquals('GuzzleHttp\Handler\MockHandler', get_class($value->getConfig('handler')));
+    }
+
+    /**
+     * @test
+     *
+     * Check verify with no params
+     */
+    public function itShouldFailRequestVerifyWithNoParams()
+    {
+        $api = new TestAPI();
+        $this->assertEquals(false, $api->verifyRequest([]));
+    }
+
+    /**
+     * @test
+     *
+     * @expectedException TypeError
+     *
+     * Check verify with no params
+     */
+    public function itShouldFailRequestVerifyWithNoParamsAgain()
+    {
+        $api = new TestAPI();
+        $this->assertEquals(false, $api->verifyRequest(null));
+    }
+
+    /**
+     * @test
+     *
+     * Check verify with params
+     */
+    public function itShouldPassRequestVerifyWithParams()
+    {
+        $hmac = '4712bf92ffc2917d15a2f5a273e39f0116667419aa4b6ac0b3baaf26fa3c4d20';
+        $params = [
+            'code'      => '0907a61c0c8d55e99db179b68161bc00',
+            'hmac'      => $hmac,
+            'shop'      => 'some-shop.myshopify.com',
+            'timestamp' => '1337178173',
+        ];
+
+        $api = new TestAPI();
+        $api->setApiSecret('hush');
+        $this->assertEquals(true, $api->verifyRequest($params));
+    }
+
+    /**
+     * @test
+     *
+     * Check verify with bad params
+     */
+    public function itShouldPassRequestVerifyWithBadParams()
+    {
+        $hmac = '4712bf92ffc2917d15a2f5a273e39f0116667419aa4b6ac0b3baaf26fa3c4d20';
+        $params = [
+        'code' => '0907a61c0c8d55e99db179b68161bc00-OOPS',
+        'hmac' => $hmac,
+        'shop' => 'some-shop.myshopify.com',
+        ];
+
+        $api = new TestAPI();
+        $api->setApiSecret('hush');
+        $this->assertEquals(false, $api->verifyRequest($params));
+    }
+
+    /**
+     * @test
+     *
+     * Should set shop and access tokeb via quick method
+     */
+    public function itShouldSetSession()
+    {
+        $api = new TestAPI();
+        $api->setSession('example.myshopify.com', '1234');
+
+        $this->assertEquals('example.myshopify.com', $api->getShop());
+        $this->assertEquals('1234', $api->getAccessToken());
+    }
+
+    /**
+     * @test
+     *
+     * Should isolate API session
+     */
+    public function itShouldWithSession()
+    {
+        $self = $this;
+        $api = new TestAPI();
+
+        // Isolated for a shop
+        $api->withSession('example.myshopify.com', '1234', function () use (&$self) {
+            $self->assertEquals('example.myshopify.com', $this->getShop());
+            $self->assertEquals('1234', $this->getAccessToken());
+        });
+
+        // Isolated for a shop
+        $api->withSession('example2.myshopify.com', '12345', function () use (&$self) {
+            $self->assertEquals('example2.myshopify.com', $this->getShop());
+            $self->assertEquals('12345', $this->getAccessToken());
+        });
+
+        // Isolated for a shop and returns a value
+        $valueReturn = $api->withSession('example2.myshopify.com', '12345', function () use (&$self) {
+            return $this->getAccessToken();
+        });
+        $this->assertEquals($valueReturn, '12345');
+
+        // Should remain untouched
+        $this->assertEquals($api->getShop(), null);
+        $this->assertEquals($api->getAccessToken(), null);
+    }
+
+    /**
+     * @test
+     * @expectedException TypeError
+     *
+     * Ensure a closure is passed to withSession
+     */
+    public function itShouldThrowExceptionForSessionWithNoClosure()
+    {
+        $api = new TestAPI();
+        $api->withSession('example.myshopify.com', '1234', null);
+    }
+
+    /**
+     * @test
+     *
+     * Should get access token from Shopify
+     */
+    public function itShouldGetAccessTokenFromShopify()
+    {
+        $response = new Response(
+            200,
+            [],
+            file_get_contents(__DIR__.'/fixtures/admin__oauth__access_token.json')
+        );
+
+        $mock = new MockHandler([$response]);
+        $client = new Client(['handler' => $mock]);
+
+        $api = new RestAPI();
+        $api->setShop('example.myshopify.com');
+        $api->setApiKey('123');
+        $api->setApiSecret('abc');
+        $api->setClient($client);
+
+        $code = '!@#';
+        $token = $api->requestAccessToken($code);
+        $data = json_decode($mock->getLastRequest()->getBody());
+
+        $this->assertEquals('f85632530bf277ec9ac6f649fc327f17', $token);
+        $this->assertEquals('abc', $data->client_secret);
+        $this->assertEquals('123', $data->client_id);
+        $this->assertEquals($code, $data->code);
+    }
+}
