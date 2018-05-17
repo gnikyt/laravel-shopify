ff75f4e0e96177423da8ad90934362b3eb0e825d
Tyler King <tyler.n.king@gmail.com>
2018-05-17 15:02:32 -0230

    Completed testing of code

--- test/GraphApiTest.php
+++ test/GraphApiTest.php
@@ -12,7 +12,7 @@ class GraphApiTest extends \PHPUnit\Framework\TestCase
     /**
      * @test
      *
-     * Checking base URL for API calls on public
+     * Checking base URL for API calls
      */
     public function itShouldReturnBaseUrl()
     {
@@ -54,10 +54,36 @@ class GraphApiTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @test
+     * @expectedException Exception
+     * @expectedExceptionMessage API password required for Shopify GraphQL calls
+     *
+     * Ensure Shopify API password is there for private calls
+     */
+    public function itShouldThrowExceptionForMissingApiPasswordOnPrivate()
+    {
+        $api = new GraphAPI(true);
+        $api->request('{}');
+    }
+
+    /**
+     * @test
+     * @expectedException Exception
+     * @expectedExceptionMessage Access token required for Shopify GraphQL calls
+     *
+     * Ensure Shopify API password is there for private calls
+     */
+    public function itShouldThrowExceptionForMissingAccessTokenOnPublic()
+    {
+        $api = new GraphAPI();
+        $api->request('{}');
+    }
+
+    /**
+     * @test
      *
      * Should get Guzzle response and JSON body
      */
-    public function itShouldReturnGuzzleResponseAndJsonBody()
+    public function itShouldReturnGuzzleResponseAndJsonBodyWithApiCallLimits()
     {
         $response = new Response(
             200,
@@ -99,5 +125,23 @@ QL;
         $this->assertEquals(true, is_object($request->body));
         $this->assertEquals('gift-card', $request->body->shop->products->edges[0]->node->handle);
         $this->assertEquals('!@#', $token_header);
+
+        $this->assertEquals(5, $api->getApiCalls('made'));
+        $this->assertEquals(1000, $api->getApiCalls('limit'));
+        $this->assertEquals(1000 - 5, $api->getApiCalls('left'));
+        $this->assertEquals(['left' => 1000 - 5, 'made' => 5, 'limit' => 1000, 'requestedCost' => 5, 'actualCost' => 5, 'restoreRate' => 50], $api->getApiCalls());
+    }
+
+    /**
+     * @test
+     * @expectedException Exception
+     * @expectedExceptionMessage Invalid API call limit key. Valid keys are: left, made, limit, restoreRate, requestedCost, actualCost
+     *
+     * Ensure we pass a valid key to the API calls
+     */
+    public function itShouldThrowExceptionForInvalidApiCallsKey()
+    {
+        $api = new GraphAPI();
+        $api->getApiCalls('oops');
     }
 }
