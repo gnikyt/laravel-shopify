3790b1856b7a3fbe6b54e6c2669ed4a0af3b06d9
Tyler King <tyler.n.king@gmail.com>
2018-09-17 10:04:49 -0230

    Possible fix for #78 by using Shopify's own JS

--- src/ShopifyApp/resources/views/auth/fullpage_redirect.blade.php
+++ src/ShopifyApp/resources/views/auth/fullpage_redirect.blade.php
@@ -1,24 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8" />
+        <meta charset="utf-8">
         <base target="_top">
 
         <title>Redirecting...</title>
 
+        <script type="text/javascript" src="https://cdn.shopify.com/s/assets/external/app.js"></script>
         <script type="text/javascript">
             // If the current window is the 'parent', change the URL by setting location.href
+            var redirectUrl = "{!! $authUrl !!}";
             if (window.top == window.self) {
-                window.top.location.href = "{!! $authUrl !!}";
-                // If the current window is the 'child', change the parent's URL with postMessage
+                window.location.assign(redirectUrl);
             } else {
-                normalizedLink = document.createElement('a');
-                normalizedLink.href = "{!! $authUrl !!}";
-                data = JSON.stringify({
-                    message: 'Shopify.API.remoteRedirect',
-                    data: { location: normalizedLink.href }
-                });
-                window.parent.postMessage(data, "https://{{ $shopDomain }}");
+                ShopifyApp.redirect(redirectUrl);
             }
         </script>
     </head>
