a00103bbf5b48912ce9f4c7eb35fb042fa2c942e
Tyler King <tyler.n.king@gmail.com>
2018-11-14 22:57:28 -0330

    Moved Shop model logic to trait

--- src/ShopifyApp/Models/Shop.php
+++ src/ShopifyApp/Models/Shop.php
@@ -3,13 +3,11 @@
 namespace OhMyBrew\ShopifyApp\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use OhMyBrew\ShopifyApp\Facades\ShopifyApp;
-use OhMyBrew\ShopifyApp\Scopes\NamespaceScope;
+use OhMyBrew\ShopifyApp\Traits\ShopModelTrait;
 
 class Shop extends Model
 {
-    use SoftDeletes;
+    use ShopModelTrait;
 
     /**
      * The attributes that are mass assignable.
@@ -39,92 +37,4 @@ class Shop extends Model
      * @var array
      */
     protected $dates = ['deleted_at'];
-
-    /**
-     * The API instance.
-     *
-     * @var object
-     */
-    protected $api;
-
-    /**
-     * The "booting" method of the model.
-     *
-     * @return void
-     */
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::addGlobalScope(new NamespaceScope());
-    }
-
-    /**
-     * Creates or returns an instance of API for the shop.
-     *
-     * @return object
-     */
-    public function api()
-    {
-        if (!$this->api) {
-            // Create new API instance
-            $api = ShopifyApp::api();
-            $api->setSession($this->shopify_domain, $this->shopify_token);
-
-            $this->api = $api;
-        }
-
-        // Return existing instance
-        return $this->api;
-    }
-
-    /**
-     * Checks is shop is grandfathered in.
-     *
-     * @return bool
-     */
-    public function isGrandfathered()
-    {
-        return ((bool) $this->grandfathered) === true;
-    }
-
-    /**
-     * Get charges.
-     *
-     * @return \Illuminate\Database\Eloquent\Collection
-     */
-    public function charges()
-    {
-        return $this->hasMany('OhMyBrew\ShopifyApp\Models\Charge');
-    }
-
-    /**
-     * Checks if charges have been applied to the shop.
-     *
-     * @return bool
-     */
-    public function hasCharges()
-    {
-        return $this->charges->isNotEmpty();
-    }
-
-    /**
-     * Gets the plan.
-     *
-     * @return \OhMyBrew\ShopifyApp\Models\Plan
-     */
-    public function plan()
-    {
-        return $this->belongsTo('OhMyBrew\ShopifyApp\Models\Plan');
-    }
-
-    /**
-     * Checks if the shop is freemium.
-     *
-     * @return bool
-     */
-    public function isFreemium()
-    {
-        return ((bool) $this->freemium) === true;
-    }
 }
