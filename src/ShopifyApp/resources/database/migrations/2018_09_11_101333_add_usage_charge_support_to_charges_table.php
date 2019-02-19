46c3164ae485344ddbe97cd49a2603a2189e27f9
Daljeet Singh <d.singh@cynux.com>
2019-02-18 21:24:34 -0800

    Missing Use Statement #181

--- src/ShopifyApp/resources/database/migrations/2018_09_11_101333_add_usage_charge_support_to_charges_table.php
+++ src/ShopifyApp/resources/database/migrations/2018_09_11_101333_add_usage_charge_support_to_charges_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class AddUsageChargeSupportToChargesTable extends Migration
 {
