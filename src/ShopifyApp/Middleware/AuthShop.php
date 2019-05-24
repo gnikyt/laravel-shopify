efbfd4d93d4f86cb063a9763f15228b5252b7ec5
Tyler King <tyler@ohmybrew.com>
2019-05-24 00:39:14 +0000

    Apply fixes from StyleCI

--- src/ShopifyApp/Middleware/AuthShop.php
+++ src/ShopifyApp/Middleware/AuthShop.php
@@ -21,15 +21,14 @@ class AuthShop
      * Handle an incoming request.
      *
      * @param \Illuminate\Http\Request $request
-     * @param \Closure $next
+     * @param \Closure                 $next
      *
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
     {
         $validation = $this->validateShop($request);
-        if ($validation !== true)
-        {
+        if ($validation !== true) {
             return $validation;
         }
 
@@ -57,8 +56,7 @@ class AuthShop
             $shop->trashed() ||
             empty($session->getToken(true)) ||
             ($shopParam && $shopParam !== $shop->shopify_domain) === true
-        )
-        {
+        ) {
             // Either no shop session or shops do not match
             $session->forget();
 
@@ -75,7 +73,7 @@ class AuthShop
      * Come back with a response.
      *
      * @param \Illuminate\Http\Request $request
-     * @param \Closure $next
+     * @param \Closure                 $next
      *
      * @return mixed
      */
@@ -86,10 +84,8 @@ class AuthShop
         if (
             Config::get('shopify-app.esdk_enabled') &&
             ($request->ajax() || $request->expectsJson() || $request->isJson()) === false
-        )
-        {
-            if (($response instanceof BaseResponse) === false)
-            {
+        ) {
+            if (($response instanceof BaseResponse) === false) {
                 // Not an instance of a Symfony response, override
                 $response = new Response($response);
             }
