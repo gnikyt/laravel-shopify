1c5095b0cb3c10318b22157ba348ee98062f92ca
Nathan Pope <ncpope@gmail.com>
2019-08-23 13:25:01 +0300

    Updated app login view to match the default app login on the shopify rails gem.

--- src/ShopifyApp/resources/views/auth/index.blade.php
+++ src/ShopifyApp/resources/views/auth/index.blade.php
@@ -1,93 +1,123 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        
-        <title>Shopify App — Login</title>
+<head>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('shopify-app.app_name') }} — Login</title>
 
-        <style>
-            html, body { padding: 0; margin: 0; }
-            body {
-                font-family: "ProximaNovaLight", "Helvetica Neue", Helvetica, Arial, sans-serif;
-                background-color: #f2f7fa;
-            }
-            h1 {
-                font-weight: 300;
-                font-size: 40px;
-                margin-bottom: 10px;
-            }
-            .subhead {
-                font-size: 17px;
-                line-height: 32px;
-                font-weight: 300;
-                color: #969A9C;
-            }
-            .subhead.error {
-                color: #f4645f;
-            }
-            input {
-                width: 300px;
-                height: 50px;
-                padding: 10px;
-                border: 1px solid #479CCf;
-                color: #575757;
-                background-color: #ffffff;
-                box-sizing: border-box;
-                border-radius: 4px 0 0 4px;
-                font-size: 18px;
-                float: left;
-            }
-            button {
-                color: #ffffff;
-                background-color: #3793cb;
-                width: 100px;
-                height: 50px;
-                padding: 10px 20px 10px 20px;
-                box-sizing: border-box;
-                border: none;
-                text-shadow: 0 1px 0 #3188bc;
-                font-size: 18px;
-                cursor: pointer;
-                border-radius: 0 4px 4px 0;
-                float: right;
-            }
-            button:hover {
-                background-color: #479CCf;
-            }
-            form {
-                display: block;
-            }
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <style>
+        * {
+            -moz-box-sizing: border-box;
+            -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+        }
+
+        body {
+            padding: 2.5em 0;
+            color: #212b37;
+            font-family: -apple-system,BlinkMacSystemFont,San Francisco,Roboto,Segoe UI,Helvetica Neue,sans-serif;
+        }
+
+        .container {
+            width: 100%;
+            text-align: center;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        @media screen and (min-width: 510px) {
             .container {
-                text-align: center;
-                margin-top: 100px;
-                padding: 20px;
+                width: 510px;
             }
-            .container__form {
-                width: 400px;
-                margin: auto;
-            }
-        </style>
-    </head>
-    <body>
-        <main class="container" role="main">
-            <header>
-                <h1>{{ config('shopify-app.app_name') }}</h1>
-                <p class="subhead">
-                    <label for="shop">Enter your Shopify domain to login.</label>
-                </p>
+        }
 
-                @if (session()->has('error'))
-                    <p class="subheadError">{{ session('error') }}</p>
-                @endif
-            </header>
+        .title {
+            font-size: 1.5em;
+            margin: 2em auto;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            word-break: break-all;
+        }
 
-            <div class="container__form">
-                <form class="form-horizontal" method="POST" action="{{ route('authenticate') }}">
-                    {{ csrf_field() }}
-                    <input type="text" name="shop" id="shop" placeholder="example.myshopify.com" value="{{ isset($shopDomain) ? $shopDomain : '' }}" autofocus="true">
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
-        </main>
-    </body>
+        .subtitle {
+            font-size: 0.8em;
+            font-weight: 500;
+            color: #64737f;
+            line-height: 2em;
+        }
+
+        .error {
+            line-height: 1em;
+            padding: 0.5em;
+            color: red;
+        }
+
+        input.marketing-input {
+            width: 100%;
+            height: 52px;
+            padding: 0 15px;
+            box-shadow: 0 0 0 1px #ddd;
+            border: 0;
+            border-radius: 5px;
+            background-color: #fff;
+            font-size: 1em;
+            margin-bottom: 15px;
+        }
+
+        input.marketing-input:focus {
+            color: #000;
+            outline: 0;
+            box-shadow: 0 0 0 2px #5e6ebf;
+        }
+
+        button.marketing-button {
+            display: inline-block;
+            width: 100%;
+            padding: 1.0625em 1.875em;
+            background-color: #5e6ebf;
+            color: #fff;
+            font-weight: 700;
+            font-size: 1em;
+            text-align: center;
+            outline: none;
+            border: 0 solid transparent;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        button.marketing-button:hover {
+            background: linear-gradient(to bottom, #5c6ac4, #4959bd);
+            border-color: #3f4eae;
+        }
+
+        button.marketing-button:focus {
+            box-shadow: 0 0 0.1875em 0.1875em rgba(94,110,191,0.5);
+            background-color: #223274;
+            color: #fff;
+        }
+    </style>
+</head>
+<body>
+
+    <main class="container" role="main">
+        <h3 class="title">
+            {{ config('shopify-app.app_name') }}
+        </h3>
+        <p class="subhead">
+            <label for="shop">Enter your Shopify domain to login.</label>
+        </p>
+
+        <form method="POST" action="{{ route('authenticate') }}">
+            @if (session()->has('error'))
+                <div class="error">{{ session('error') }}</div>
+            @endif
+            <input id="shop" name="shop" type="text" autofocus="autofocus" placeholder="example.myshopify.com" class="marketing-input">
+            <button type="submit" class="marketing-button">Install</button>
+        </form>
+    </main>
+
+</body>
 </html>
