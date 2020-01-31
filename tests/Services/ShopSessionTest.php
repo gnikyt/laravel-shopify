f5d3b9c36307a79c02f9e9f9fd8fd72ef66f1303
Darryn Ten <darryn.ten@gmail.com>
2020-01-31 19:08:13 +0700

    Unit tests

--- tests/Services/ShopSessionTest.php
+++ tests/Services/ShopSessionTest.php
@@ -118,4 +118,38 @@ class ShopSessionTest extends TestCase
         $this->assertNull(Session::get(ShopSession::TOKEN));
         $this->assertNull(Session::get(ShopSession::DOMAIN));
     }
+
+    public function testSameSiteCookie()
+    {
+        $incompatibleUserAgents = [
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) GSA/87.0.279142407 Mobile/15E148 Safari/605.1',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Safari/605.1.15',
+            'Mozilla/5.0 (Linux; U; Android 7.0; en-US; SM-G935F Build/NRD90M) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/11.3.8.976 U3/0.8.0 Mobile Safari/534.30',
+        ];
+
+        $compatibleUserAgents = [
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:72.0) Gecko/20100101 Firefox/72.0',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Safari/605.1.15',
+        ];
+
+        // Set a shop
+        $shop = factory(Shop::class)->create();
+
+        foreach ($incompatibleUserAgents as $agent) {
+            $_SERVER['HTTP_USER_AGENT'] = $agent;
+            $response = $this->get('/');
+
+            $this->assertNull($response->baseResponse->headers->getCookies()[0]->getSameSite());
+            $this->assertFalse($response->baseResponse->headers->getCookies()[0]->isSecure());
+        }
+
+        foreach ($compatibleUserAgents as $agent) {
+            $_SERVER['HTTP_USER_AGENT'] = $agent;
+            $response = $this->get('/');
+
+            $this->assertEquals('none', $response->baseResponse->headers->getCookies()[0]->getSameSite());
+            $this->assertTrue($response->baseResponse->headers->getCookies()[0]->isSecure());
+        }
+    }
 }
