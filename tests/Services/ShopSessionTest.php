48bd3a3891161d42ce30e6832eeb14f01fd461ad
Darryn Ten <darryn.ten@gmail.com>
2020-01-31 19:23:08 +0700

    Add tests for junk user agents

--- tests/Services/ShopSessionTest.php
+++ tests/Services/ShopSessionTest.php
@@ -133,8 +133,23 @@ class ShopSessionTest extends TestCase
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Safari/605.1.15',
         ];
 
+        $badUserAgents = [
+            'XXXX',
+            null,
+            '15.15.15',
+            'firefox 1.0',
+        ];
+
         // Set a shop
         $shop = factory(Shop::class)->create();
+
+        foreach ($badUserAgents as $agent) {
+            $_SERVER['HTTP_USER_AGENT'] = $agent;
+            $response = $this->get('/');
+
+            $this->assertNull($response->baseResponse->headers->getCookies()[0]->getSameSite());
+            $this->assertFalse($response->baseResponse->headers->getCookies()[0]->isSecure());
+        }
 
         foreach ($incompatibleUserAgents as $agent) {
             $_SERVER['HTTP_USER_AGENT'] = $agent;
