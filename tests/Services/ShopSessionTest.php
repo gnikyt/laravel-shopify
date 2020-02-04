1853ea8299399db62bee646f0f9dce9f142f2b29
Daniel Tsou <daniel@22media.co>
2020-02-04 14:31:46 +0800

    Added a compatible user agent that was previously reported as failing

--- tests/Services/ShopSessionTest.php
+++ tests/Services/ShopSessionTest.php
@@ -130,6 +130,7 @@ class ShopSessionTest extends TestCase
         ];
 
         $compatibleUserAgents = [
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.78 Safari/537.36',
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:72.0) Gecko/20100101 Firefox/72.0',
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Safari/605.1.15',
