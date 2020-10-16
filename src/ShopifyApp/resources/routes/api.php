70eff29a80cc7bbd823dd1ca95a244e3f0f18f98
Scott Carpenter <scott@payforstay.com>
2020-10-16 16:24:18 -0600

    Use the config route names when registering routes

--- src/ShopifyApp/resources/routes/api.php
+++ src/ShopifyApp/resources/routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
+use Osiset\ShopifyApp\Services\ConfigHelper;
 use function Osiset\ShopifyApp\registerPackageRoute;
 
 // Check if manual routes override is to be use
-$manualRoutes = Config::get('shopify-app.manual_routes');
+$manualRoutes = ConfigHelper::get('manual_routes');
 
 if ($manualRoutes) {
     // Get a list of route names to exclude
@@ -56,6 +56,6 @@ Route::group(['middleware' => ['api']], function ($manualRoutes) {
             'Osiset\ShopifyApp\Http\Controllers\WebhookController@handle'
         )
         ->middleware('auth.webhook')
-        ->name('webhook');
+        ->name(ConfigHelper::get('route_names.webhook'));
     }
 });
