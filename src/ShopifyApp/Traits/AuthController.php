cec02d12ec47f65409c16fd84b62e34bf7c39663
Vitaliy Dubov <vdubov@simtechdev.org>
2021-05-07 05:02:11 +0400

    Redirect if the user clicked on any link before load Turbo

--- src/ShopifyApp/Traits/AuthController.php
+++ src/ShopifyApp/Traits/AuthController.php
@@ -26,7 +26,13 @@ trait AuthController
     public function authenticate(Request $request, AuthenticateShop $authShop)
     {
         // Get the shop domain
-        $shopDomain = ShopDomain::fromNative($request->get('shop'));
+        if (getShopifyConfig('turbo_enabled')) {
+            // If the user clicked on any link before load Turbo and receiving the token
+            $shopDomain = $request->user()->getDomain();
+            $request['shop'] = $shopDomain->toNative();
+        } else {
+            $shopDomain = ShopDomain::fromNative($request->get('shop'));
+        }
 
         // Run the action
         [$result, $status] = $authShop($request);
