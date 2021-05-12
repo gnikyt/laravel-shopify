e969b4a2e97359cc40c2d7d9d7c7ef133501fb53
Vitaliy Dubov <vdubov@simtechdev.org>
2021-05-12 19:31:25 +0400

    changed routes

--- src/ShopifyApp/resources/routes/shopify.php
+++ src/ShopifyApp/resources/routes/shopify.php
@@ -135,4 +135,13 @@ Route::group(['prefix' => getShopifyConfig('prefix'), 'middleware' => ['web']], 
         ->middleware(['verify.shopify'])
         ->name(getShopifyConfig('route_names.billing.usage_charge'));
     }
+
+    if (registerPackageRoute('billing.domain', $manualRoutes)) {
+        Route::get(
+            '/billing/domain',
+            'Osiset\ShopifyApp\Http\Controllers\BillingController@processDomain'
+        )
+        ->middleware(['verify.shopify'])
+        ->name(getShopifyConfig('route_names.billing.domain'));
+    }
 });
