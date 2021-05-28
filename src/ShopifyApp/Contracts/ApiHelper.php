8b0682df8b677bff7445091a32022d5beb14c184
Onur Kose <onur.kose@despatchcloud.com>
2021-05-28 14:40:37 +0300

    Convert int type to string for shopify webhooks

--- src/ShopifyApp/Contracts/ApiHelper.php
+++ src/ShopifyApp/Contracts/ApiHelper.php
@@ -167,11 +167,11 @@ interface ApiHelper
     /**
      * Delete a webhook.
      *
-     * @param int $webhookId The webhook ID to delete.
+     * @param string $webhookId The webhook ID to delete.
      *
      * @return void
      */
-    public function deleteWebhook(int $webhookId);
+    public function deleteWebhook(string $webhookId);
 
     /**
      * Creates a usage charge for a recurring charge.
