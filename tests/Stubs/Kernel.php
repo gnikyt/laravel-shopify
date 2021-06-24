bedf7e8366add830208cec21bbb26d28d4f75e99
Tyler King <tyler@osiset.com>
2021-06-24 15:16:25 -0230

    Fix to Kernel testcase referring to old middleware

--- tests/Stubs/Kernel.php
+++ tests/Stubs/Kernel.php
@@ -9,10 +9,9 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Routing\Middleware\ThrottleRequests;
 use Orchestra\Testbench\Http\Middleware\RedirectIfAuthenticated;
 use Osiset\ShopifyApp\Http\Middleware\AuthProxy;
-use Osiset\ShopifyApp\Http\Middleware\AuthShopify;
-use Osiset\ShopifyApp\Http\Middleware\AuthToken;
 use Osiset\ShopifyApp\Http\Middleware\AuthWebhook;
 use Osiset\ShopifyApp\Http\Middleware\Billable;
+use Osiset\ShopifyApp\Http\Middleware\VerifyShopify;
 
 class Kernel extends \Orchestra\Testbench\Http\Kernel
 {
@@ -32,10 +31,9 @@ class Kernel extends \Orchestra\Testbench\Http\Kernel
         'throttle'   => ThrottleRequests::class,
 
         // Added for testing
-        'auth.shopify' => AuthShopify::class,
-        'auth.token'   => AuthToken::class,
-        'auth.webhook' => AuthWebhook::class,
-        'auth.proxy'   => AuthProxy::class,
-        'billable'     => Billable::class,
+        'verify.shopify' => VerifyShopify::class,
+        'auth.webhook'   => AuthWebhook::class,
+        'auth.proxy'     => AuthProxy::class,
+        'billable'       => Billable::class,
     ];
 }
