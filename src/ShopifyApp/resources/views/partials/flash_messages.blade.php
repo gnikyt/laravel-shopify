8f8653d7a9e8358c4e4af485c8c27e3d4da71493
Tyler King <tyler@osiset.com>
2021-07-05 17:42:32 -0230

    Change flash notice variable naming to not clash. (#864)

--- src/ShopifyApp/resources/views/partials/flash_messages.blade.php
+++ src/ShopifyApp/resources/views/partials/flash_messages.blade.php
@@ -2,17 +2,17 @@
     document.addEventListener('DOMContentLoaded', function () {
         var Toast = actions.Toast;
 
-        @if (request()->has('notice'))
+        @if (request()->has('notice') || isset($flashNotice))
             var toastNotice = Toast.create(app, {
-                message: "{{ request()->get('notice') }}",
+                message: "{{ request()->get('notice', $flashNotice ?? null) }}",
                 duration: 3000,
             });
             toastNotice.dispatch(Toast.Action.SHOW);
         @endif
 
-        @if (request()->has('error'))
+        @if (request()->has('error') || isset($flashError))
             var toastNotice = Toast.create(app, {
-                message: "{{ request()->get('error') }}",
+                message: "{{ request()->get('error', $flashError ?? null) }}",
                 duration: 3000,
                 isError: true,
             });
