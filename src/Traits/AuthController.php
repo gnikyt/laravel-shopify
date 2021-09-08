2c865683f53b13b045ff06160a349b614a8ed1e3
Drilon S <lonnchee@gmail.com>
2021-09-08 17:44:56 +0200

    Update AuthController.php

--- src/Traits/AuthController.php
+++ src/Traits/AuthController.php
@@ -82,7 +82,7 @@ trait AuthController
         if ($query) {
             // remove "token" from the target's query string
             $params = Util::parseQueryString($query);
-            $params['shop'] ??= $shopDomain->toNative() ?? '';
+            $params['shop'] = $params['shop'] ?? $shopDomain->toNative() ?? '';
             unset($params['token']);
 
             $cleanTarget = trim(explode('?', $target)[0].'?'.http_build_query($params), '?');
