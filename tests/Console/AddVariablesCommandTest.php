c7faec1f11497e356e03b97715615e857686a8e4
Vitaly <vita-dubov@yandex.ru>
2022-03-11 01:44:27 +0400

    update test

--- tests/Console/AddVariablesCommandTest.php
+++ tests/Console/AddVariablesCommandTest.php
@@ -2,13 +2,29 @@
 
 namespace Osiset\ShopifyApp\Test\Console;
 
-use Osiset\ShopifyApp\Test\TestCase;
+use Tests\TestCase;
 
-class AddVariablesCommandTest extends TestCase
+class TestTest extends TestCase
 {
     public function testItShouldRun(): void
     {
         $this->artisan('shopify-app:add:variables')
+            ->expectsQuestion('This will invalidate SHOPIFY_APP_NAME variable. Are you sure you want to override SHOPIFY_APP_NAME?', 'yes')
+            ->expectsQuestion('This will invalidate SHOPIFY_API_KEY variable. Are you sure you want to override SHOPIFY_API_KEY?', 'yes')
+            ->expectsQuestion('This will invalidate SHOPIFY_API_SECRET variable. Are you sure you want to override SHOPIFY_API_SECRET?', 'yes')
+            ->expectsQuestion('This will invalidate SHOPIFY_API_SCOPES variable. Are you sure you want to override SHOPIFY_API_SCOPES?', 'yes')
+            ->expectsQuestion('This will invalidate SHOPIFY_REDIRECT_AFTER_SUCCESS_CHARGE variable. Are you sure you want to override SHOPIFY_REDIRECT_AFTER_SUCCESS_CHARGE?', 'yes')
+            ->expectsQuestion('This will invalidate AFTER_AUTHENTICATE_JOB variable. Are you sure you want to override AFTER_AUTHENTICATE_JOB?', 'yes')
             ->assertExitCode(0);
+    }
+
+    public function testItShouldRunForce(): void
+    {
+        $this->artisan('shopify-app:add:variables --force')->assertExitCode(0);
+    }
+
+    public function testItShouldRunAlwaysNo(): void
+    {
+        $this->artisan('shopify-app:add:variables --always-no')->assertExitCode(0);
     }
 }
