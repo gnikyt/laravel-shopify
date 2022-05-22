b00f5cf1d4cc2f336c24651f8bd37e63659f23b7
Enmaboya <vita-dubov@yandex.ru>
2022-05-22 15:51:39 +0400

    add theme edit scope and theme support configs

--- src/resources/config/shopify-app.php
+++ src/resources/config/shopify-app.php
@@ -197,7 +197,7 @@ return [
     |
     */
 
-    'api_scopes' => env('SHOPIFY_API_SCOPES', 'read_products,write_products'),
+    'api_scopes' => env('SHOPIFY_API_SCOPES', 'read_products,write_products,read_theme'),
 
     /*
     |--------------------------------------------------------------------------
@@ -470,5 +470,28 @@ return [
         * The table name for Plan model.
         */
         'plans' => 'plans',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Checking theme compatibility
+    |--------------------------------------------------------------------------
+    |
+    | It is necessary to check if your application is compatible with the theme app blocks.
+    |
+    */
+    'theme_support' => [
+        /**
+         * Specify the name of the template the app will integrate with
+         */
+        'templates' => ['product', 'collection', 'index'],
+        /**
+         * Interval for caching the request: minutes, seconds, hours, days, etc.
+         */
+        'cache_interval' => 'hours',
+        /**
+         * Cache duration
+         */
+        'cache_duration' => '12'
     ]
 ];
