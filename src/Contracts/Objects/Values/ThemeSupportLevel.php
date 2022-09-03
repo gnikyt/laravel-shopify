4d69f352c4b1ba2284f958f04d42ba7bf78b146d
vdubov <vitaliy.dubov@bolide.ai>
2022-09-04 02:55:52 +0400

    add contracts

---
+++ src/Contracts/Objects/Values/ThemeSupportLevel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Osiset\ShopifyApp\Contracts\Objects\Values;
+
+use Funeralzone\ValueObjects\ValueObject;
+
+/**
+ * Access token value object.
+ */
+interface ThemeSupportLevel extends ValueObject
+{
+}
