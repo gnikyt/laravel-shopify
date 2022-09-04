bc26fc5ddc9aad6428e097edac66c43fa82e115b
vdubov <vitaliy.dubov@bolide.ai>
2022-09-04 18:10:34 +0400

    update theme support test

--- tests/Actions/VerifyThemeSupportTest.php
+++ tests/Actions/VerifyThemeSupportTest.php
@@ -2,12 +2,12 @@
 
 namespace Osiset\ShopifyApp\Test\Actions;
 
-use Osiset\ShopifyApp\Actions\VerifyThemeSupport;
-use Osiset\ShopifyApp\Contracts\Queries\Shop as IShopQuery;
-use Osiset\ShopifyApp\Objects\Enums\ThemeSupportLevel;
-use Osiset\ShopifyApp\Objects\Values\ShopId;
-use Osiset\ShopifyApp\Services\ThemeHelper;
 use Osiset\ShopifyApp\Test\TestCase;
+use Osiset\ShopifyApp\Services\ThemeHelper;
+use Osiset\ShopifyApp\Objects\Values\ShopId;
+use Osiset\ShopifyApp\Actions\VerifyThemeSupport;
+use Osiset\ShopifyApp\Objects\Enums\ThemeSupportLevel;
+use Osiset\ShopifyApp\Contracts\Queries\Shop as IShopQuery;
 
 class VerifyThemeSupportTest extends TestCase
 {
@@ -38,14 +38,7 @@ class VerifyThemeSupportTest extends TestCase
     public function testStoreWithFullExtensionSupport(): void
     {
         $shop = factory($this->model)->create();
-        $defaultThemeResponse = [0, 1, 2, 3];
-
-        $themeHelperStub = $this->createStub(ThemeHelper::class);
-        $themeHelperStub->method('themeIsReady')->willReturn(true);
-        $themeHelperStub->method('templateJSONFiles')->willReturn($defaultThemeResponse);
-        $themeHelperStub->method('mainSections')->willReturn($defaultThemeResponse);
-        $themeHelperStub->method('sectionsWithAppBlock')->willReturn($defaultThemeResponse);
-
+        $themeHelperStub = $this->createThemeHelperStub(ThemeSupportLevel::FULL);
         $action = new VerifyThemeSupport($this->app->make(IShopQuery::class), $themeHelperStub);
 
         $result = call_user_func(
@@ -60,14 +53,7 @@ class VerifyThemeSupportTest extends TestCase
     public function testStoreWithPartialExtensionSupport(): void
     {
         $shop = factory($this->model)->create();
-        $defaultThemeResponse = [0, 1, 2, 3];
-
-        $themeHelperStub = $this->createStub(ThemeHelper::class);
-        $themeHelperStub->method('themeIsReady')->willReturn(true);
-        $themeHelperStub->method('templateJSONFiles')->willReturn($defaultThemeResponse);
-        $themeHelperStub->method('mainSections')->willReturn($defaultThemeResponse);
-        $themeHelperStub->method('sectionsWithAppBlock')->willReturn([...$defaultThemeResponse, random_int(1, 99)]);
-
+        $themeHelperStub = $this->createThemeHelperStub(ThemeSupportLevel::PARTIAL);
         $action = new VerifyThemeSupport($this->app->make(IShopQuery::class), $themeHelperStub);
 
         $result = call_user_func(
@@ -82,14 +68,7 @@ class VerifyThemeSupportTest extends TestCase
     public function testStoreWithoutExtensionSupport(): void
     {
         $shop = factory($this->model)->create();
-        $defaultThemeResponse = [];
-
-        $themeHelperStub = $this->createStub(ThemeHelper::class);
-        $themeHelperStub->method('themeIsReady')->willReturn(true);
-        $themeHelperStub->method('templateJSONFiles')->willReturn($defaultThemeResponse);
-        $themeHelperStub->method('mainSections')->willReturn($defaultThemeResponse);
-        $themeHelperStub->method('sectionsWithAppBlock')->willReturn($defaultThemeResponse);
-
+        $themeHelperStub = $this->createThemeHelperStub(ThemeSupportLevel::UNSUPPORTED);
         $action = new VerifyThemeSupport($this->app->make(IShopQuery::class), $themeHelperStub);
 
         $result = call_user_func(
@@ -99,5 +78,33 @@ class VerifyThemeSupportTest extends TestCase
 
         $this->assertNotNull($result);
         $this->assertEquals(ThemeSupportLevel::UNSUPPORTED, $result);
+    }
+
+    /**
+     * Create ThemeHelper stub
+     *
+     * @param int $level
+     * @return ThemeHelper
+     */
+    protected function createThemeHelperStub(int $level): ThemeHelper
+    {
+        $themeHelperStub = $this->createStub(ThemeHelper::class);
+
+        $defaultThemeResponse = [];
+
+        if ($level === ThemeSupportLevel::FULL) {
+            $defaultThemeResponse = [0, 1, 2, 3];
+        }
+
+        $themeHelperStub->method('themeIsReady')->willReturn(true);
+        $themeHelperStub->method('templateJSONFiles')->willReturn($defaultThemeResponse);
+        $themeHelperStub->method('mainSections')->willReturn($defaultThemeResponse);
+        $themeHelperStub->method('sectionsWithAppBlock')->willReturn(
+            $level === ThemeSupportLevel::PARTIAL
+            ? [...$defaultThemeResponse, random_int(1, 99)]
+            : $defaultThemeResponse
+        );
+
+        return $themeHelperStub;
     }
 }
