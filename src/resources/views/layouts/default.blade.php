25c0d99e2e837641a3521c5cea7c38b669d06c5e
Alex <23384595+bugfixman@users.noreply.github.com>
2022-11-10 18:51:59 +0600

    Update default.blade.php

--- src/resources/views/layouts/default.blade.php
+++ src/resources/views/layouts/default.blade.php
@@ -32,7 +32,7 @@
                 var app = createApp({
                     apiKey: "{{ \Osiset\ShopifyApp\Util::getShopifyConfig('api_key', $shopDomain ?? Auth::user()->name ) }}",
                     shopOrigin: "{{ $shopDomain ?? Auth::user()->name }}",
-                    host: "{{ \Request::get('host') }}",
+                    host: "{{ \session('host') }}",
                     forceRedirect: true,
                 });
             </script>
